a9eca94ddba3722547921dbad3fbc1c72468f2ee
lauraasld <laura.schlauder@viacesi.fr>
2016-12-13 10:43:56 +0100

    commandes lecture clavier

---
+++ commandes.h
@@ -0,0 +1,21 @@
+#ifndef COMMANDES_H_INCLUDED
+#define COMMANDES_H_INCLUDED
+
+#include <termios.h>
+#include <unistd.h>
+#include <stdio>
+
+int commandeClavier(void){
+    struct termios ancienTerm, nouveauTerm;
+    int touche;
+    tcgetattr(STDIN_FILENO, &ancienTerm);// on récupère les infos du terminal
+    nouveauTerm = ancienTerm;
+    nouveauTerm.c_lflag &= ~ICANON;// on passe le terminal en mode canonique
+    nouveauTerm.c_lflag &= ~ECHO; // on passe le terminal en mode non-echo
+    tcsetattr(STDIN_FILENO, TCSANOW, &nouveauTerm); // on met en place les nouvelles infos du terminal
+    ch = getchar(); // getchar() permet de lire une touche du clavier
+    tcsetattr(STDIN_FILENO, TCSANOW, &ancienTerm); // on met en place les infos dans l'ancien terminal
+    return touche; // la fonction renvoie la touche appuyée sur le clavier
+}
+
+#endif // COMMANDES_H_INCLUDED
