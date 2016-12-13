ec9b56f1ddea38e0fe14f0516ae9f2166f0943e0
LaureAnneMary <laureanne.marechal@viacesi.fr>
2016-12-13 15:25:13 +0100

    Fonction pour ecrire l'historique

--- historique.h
+++ historique.h
@@ -1,29 +1,42 @@
 #ifndef HISTORIQUE_H_INCLUDED
 #define HISTORIQUE_H_INCLUDED
-
-void sauvegarde(int n){
-        FILE* fichier = NULL;
-        time_t secondes;
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+typedef struct Historique Historique;
+struct Historique{
         struct tm instant;
+        int typeEcran;
+        char nomImage;
+        int hauteur;
+        int longueur;
+        int positionX;
+        int positionY;
+        };
+
+void sauvegarde(int n ){
+        Historique historique;
+        historique.typeEcran=n;
+        time_t seconde;
+        time(&seconde);
+        historique.instant = *localtime(&seconde);
+         FILE* fichier = NULL;
         fichier=fopen("historique.txt","w");
         if(fichier!=NULL){
-        fprintf(fichier,"%d/%d/%d  %d:%d:%d, %d",instant.tm_mday, instant.tm_mon+1,instant.tm_year,i$
+                fprintf(fichier,"%d/%d/%d  %d:%d:%d",instant.tm_mday, instant.tm_mon+1,instant.tm_year,instant.tm_hour,instant.tm_min,instant.tm_sec);
+                if(historique.typeEcran==1){
+                        fprintf(fichier,"Statique");
+                        fprintf(fichier,"%c\n",historique.nomImage);
+                }else if(historique.typeEcran==2){
+                        fprintf(fichier,"Dynamique");
+                        fprintf(fichier,"%d*%d\n",historique.hauteur,historique.longueur)
+                }else if(historique.typeEcran==3){
+                        fprintf(fichier,"Interactif");
+                        fprintf(fichier,"%d*%d\n",positionX, positionY);
+
         }
         fclose(fichier);
 }
-
-void afficherhistorique(){
-        FILE* fichier=NULL;
-        char texte[7]="";
-        fichier=fopen("historique.txt","r");
-        if(fichier!=NULL){
-                fscanf(fichier,"%d %d %d %d %d %d %d", %texte[0], %texte[1], %texte[2], %texte[3], %$
-                printf("%d/%d/%d  %d:%d:%d, %d",texte[0],texte[1],texte[2],texte[3],texte[4],texte[5$
-        }
-        fclose(fichier);
-}
-
-
-
 
 #endif // HISTORIQUE_H_INCLUDED
