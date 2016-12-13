395e97fa6b578f88be479d2b955d922f5f6b6aec
LaureAnneMary <laureanne.marechal@viacesi.fr>
2016-12-13 09:18:38 +0100

    Lanceur et fonction historique

--- Lanceur.c
+++ Lanceur.c
@@ -1,46 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
+int  processusfils(){
+        int n;
+        srand(time(NULL));
+        n=rand()%3+1;
+       // while(1){
+                switch(n){
+                        case 1:
+                                printf("je suis l'ecran statique\n");
+                                //execl("ecranStatique","",NULL);
+                                break;
+                        case 2:
+                                printf("je suis l'ecran dynamique\n");
+                                //execl("ecranDynamique","",NULL);
+                                break;
+                        case 3:
+                                printf("je suis l'ecran interactif\n");
+                                //execl("ecranInteractif","",NULL);
+                                break;
+                }
+       // }
+	return n;
+}
 
+void sauvegarde(int n){
+	FILE* fichier = NULL;
+	time_t secondes;
+	struct tm instant;
+	fichier=fopen("historique.txt","w");
+	if(fichier!=NULL){
+	fprintf(fichier,"%d/%d/%d  %d:%d:%d, %d",instant.tm_mday, instant.tm_mon+1,instant.tm_year,instant.tm_hour, instant.tm_min, instant.tm_sec, n); 
+	}
+	fclose(fichier);
+}
+void afficherhistorique(){
+	FILE* fichier=NULL;
+	char texte[7]="";
+	fichier=fopen("historique.txt","r");
+	if(fichier!=NULL){
+		fscanf(fichier,"%d %d %d %d %d %d %d", %texte[0], %texte[1], %texte[2], %texte[3], %texte[4], %texte[5], %texte[6]);
+		printf("%d/%d/%d  %d:%d:%d, %d",texte[0],texte[1],texte[2],texte[3],texte[4],texte[5],texte[6]);
+	}
+	fclose(fichier);
+}
 void main(){
-	lance la veille
-	// idée -> sion touche la touche espace la fonction nous renvois 1 
-	// int n;
-	//n=toucheespace();
-	// while (n!=1);
-	int pid=creationprocessus();
+	//lance la veille
+	int n;
+	int pid=fork();
 	switch(pid){
-	case -1: 
+	case -1:
 		printf("Erreur lors de la création");
 		break;
 	case 0:
-		processusfils();
-		break;
-	default:
-		processuspère();
+
+		n=processusfils();
+		sauvegarde(n);
 		break;
 	}
+	afficherhistorique();
 }
 
-void processusfils(){
-	int n;
-	srand(time(NULL));
-	n=rand()%3+1;
-	while(1){
-		switch(n){
-			case 1:
-				execl("ecranStatique","",NULL);
-				break;
-			case 2:
-				execl("ecranDynamique","",NULL);
-				break;
-			case 3:
-				execl("ecranInteractif","",NULL);
-				break;
-		}
-	}
-}
-
-void processuspère(){
-
-}
