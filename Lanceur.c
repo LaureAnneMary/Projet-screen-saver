088c46bb946d5889360df691747eafba5b35039a
LaureAnneMary <laureanne.marechal@viacesi.fr>
2016-12-14 20:07:23 +0100

    Lanceur mis a jour

--- Lanceur.c
+++ Lanceur.c
@@ -6,62 +6,72 @@
 #include <sys/types.h>
 #include <dirent.h>
 
-int  processusfils(){
+void processusfils(){
         int n;
-	int j;
-	int i;
-	srand(time(NULL));
-	j=rand()%5+1;
+        int j;
+        int i;
+        char* str;
+ 	    char chemin[1024];
+ 	    char chemin2[1024];//declaration de deux tableaux de caractères
+        struct dirent* fichierLu=NULL;
+        srand(time(NULL));
+        j=rand()%5+1;//tirage aléatoire d'un nombre entre 1 et 5
         srand(time(NULL));
         n=rand()%3+1;
-       // while(1){
+        while(1){
                 switch(n){
                         case 1:
-				struct dirent* fichierLu=NULL;//declaration d'un pointeur vers la structure dirent
-				char* chemin;
-				chemin=getenv("EXIASAVER1_PBM");// on donne a la variable chemin le chemin vers le repertoire qui contient les images
-				DIR* ouvrir;//pointeur
-				ouvrir=opendir(chemin);//le pointeur va pointer vers le premier fichier du repertoire
-				if(ouvrir!=NULL){
-					for(i=0;i<j;i++){
-						fichierLu=readdir(ouvrir);
-					}
-					char* argument[]={"statique",fichierLu->d_name};
-					execl("EXIASAVER_HOME/ecranStatique",argument,NULL);
-				}
-				sauvegarde (n,argument);
-				closedir(ouvrir);
-				break;
+                                str=getenv("EXIASAVER1_PBM");//str va recuperer le chemin de la variable d'environnement
+				if(str != NULL) strcpy(chemin, str);//on copie la valeur de str dans chemin si cela-ci n'est pas vide
+				else getcwd(chemin, 1024); //sinon on donne autre chose(a rechercher)
+				str=getenv("EXIASAVER_HOME");//str va chercher le chemin dans EXIASAVER_HOME qui mene aux executables
+				if(str != NULL) strcpy(chemin2, str);//on copie le chemin de str dans le variable chemin2
+				else getcwd(chemin2, 1024);//sinon str est vide on donne autre chose au tableau chemin2
+                                DIR* ouvrir;//pointeur
+                                ouvrir=opendir(chemin);//le pointeur va pointer vers le premier fichier du repertoire
+                                if(ouvrir!=NULL){
+                                        for(i=0;i<j;i++){//se deplace jusqu'a j ème fichier pour le lire
+                                                fichierLu=readdir(ouvrir);//ouverture du fichier
+                                        }
+                                        char* argument[]={"statique",fichierLu->d_name,NULL};// dans un tableau de caractère je met le type d'ecran de veille lancer ainsi que le nom de fichier pbm a ouvrir.
+                                        strcat(chemin2, "/ecranStatique");//on copie a la suite du chemin que contient deja le tableau /ecranStatique
+					execl(chemin2,argument);// on execute le programme ecranStatique en envoyant en paramètre le chemin vers le fichier a executer avec des paramètres notamment le nom de l'image
+                                }
+                                sauvegarde (n,j);//fonction qui ecrit dans l'historique
+                                closedir(ouvrir);//on ferme le fichier que l'on a ouvert
+                                break;
                         case 2:
-				char* argument[]="";
-				sauvegarde(n, argument);
+                                j=0;
+                                sauvegarde(n,j );
                                // execl("ecranDynamique","",NULL);
-				printf("je suis l'ecran dynamique\n");
+                                printf("je suis l'ecran dynamique\n");
                                 break;
                         case 3:
-				char* argument[]="";
-				sauvegarde(n,argument);
+                                j=0;
+                                sauvegarde(n,j);
                                 //execl("ecranInteractif","",NULL);
-				printf("je suis l'ecran interactif\n");
+                                printf("je suis l'ecran interactif\n");
                                 break;
                 }
-       // }
-	return n;
+       }
 }
-
-void main(){
-	//lance la veille
+void main(int argc, char *argv[])
+{
 	system("clear");
-	int n;
-	int pid=fork();
-	switch(pid){
-	case -1:
-		printf("Erreur lors de la création");
-		break;
-	case 0:
-
-		processusfils();
-		break;
+	if(argc==2){
+		if(strcmp(argv[1],"-stats")==0){
+			execl("lectureHistorique","",NULL);
+		}
+	}else{
+		int n;
+		int pid=fork();
+		switch(pid){
+		case -1:
+			printf("Erreur lors de la création");
+			break;
+		case 0:
+			processusfils();
+			break;
+		}
 	}
 }
-
