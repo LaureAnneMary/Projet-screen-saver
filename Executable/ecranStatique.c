6d530706704d7a1ab363b30762c34adf6cdd3de4
LaureAnneMary <laureanne.marechal@viacesi.fr>
2016-12-15 13:58:45 +0100

    Affichage de l'ecran Statique

--- Executable/ecranStatique.c
+++ Executable/ecranStatique.c
@@ -8,16 +8,31 @@
 int main(int argc, char *argv[])
 {
     Pbm image; // "image" va contenir toutes nos variables situées dans la structure Pbm
-    
+    char chemin_pbm[1024];
     int tableauPourImage[80][24] = {0}; // on créer un tableau à deux dimensions rempli de 0
-    if(argv[1]==NULL){
-    choixAleatoireImage(&image); // on va choisir une image aléatoirement, on envoie "&image" pour u$
-    }else{
-    image.nomImage = argv[1];
+    if(argc==1)
+  {
+        StatiquechoixAleatoireImage(&image); // on va choisir une image aléatoirement, on envoie "&image" pour u$
+    }
+    else
+    {
+        char* argument=argv[1];
+        strcpy(image.nomImage, argument);
+        char* str;
+        str=getenv("EXIASAVER1_PBM");
+        if (str != NULL)
+        {
+            strcpy(chemin_pbm, str);//on copie la valeur de str dans chemin si cela-ci n'est pas vide
+        }
+        else
+        {
+            getcwd(chemin_pbm, 1024);
+        }
+        strcat(chemin_pbm,argument);
     }
     FILE* fichier = NULL; // notre "FILE*" permet de récupérer le pointeur du fichier pour ensuite p$
 
-    fichier = fopen(image.nomImage, "r"); // on ouvre le fichier qui est contenu dans le char de not$
+    fichier = fopen(chemin_pbm, "r"); // on ouvre le fichier qui est contenu dans le char de not$
 
     if(fichier != NULL) // peremt de s'assurer que le fichier s'est bien ouvert
     {
@@ -35,6 +50,3 @@ int main(int argc, char *argv[])
 
     return 0;
 }
-
-
-
