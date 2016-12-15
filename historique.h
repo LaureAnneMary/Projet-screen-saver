0f3978b738ccf4d64359a1606328fa80f0d22418
LaureAnneMary <laureanne.marechal@viacesi.fr>
2016-12-15 11:10:14 +0100

    Historique reussi

--- historique.h
+++ historique.h
@@ -14,10 +14,9 @@ void sauvegarde(int n,int j){
         struct tm tm_maintenant=*localtime(&maintenant);/// avoir l'heure locale
         char s_maintenant[sizeof "HH:MM:SS"];//stocker l'heure dans un tableau
         strftime(s_maintenant, sizeof s_maintenant,"%H:%M:%S",&tm_maintenant);
-         FILE* fichier = NULL;//initaialisation d'un pointeur a null
-        fichier=fopen("historique.txt","a++");//ouverture ou creation si il n'existe pas d'un fichie$
+        FILE* fichier = NULL;//initaialisation d'un pointeur a null
+        fichier=fopen("historique.txt","a++");//ouverture ou creation si il n'existe pas d'un fichier texte historique
         if(fichier!=NULL){
-                fprintf(fichier,"%s",s_maintenant);//on stocke le tableau d'heure
                 if(typeEcran==1){// si ecran statique
                          int i;
                         struct dirent* fichierLu=NULL;
@@ -28,20 +27,20 @@ void sauvegarde(int n,int j){
                                          fichierLu=readdir(ouvrir);
                                 }
                         }
-                        fprintf(fichier,"statique");//on stocke le type d'ecran de veille
-                        fprintf(fichier,"%s\n",fichier->d_image);// on stocke le nom de l'image
+                        fprintf(fichier," 1 ");//on stocke le type d'ecran de veille
+                        fprintf(fichier," %s ",fichierLu->d_name);// on stocke le nom de l'image
+                        fprintf(fichier," %s\n ",s_maintenant);//on stocke le tableau d'heure
                 }else if(typeEcran==2){
                         fprintf(fichier,"dynamique");//on stocke le type dynamqiue
-                        fprintf(fichier,"5x3\n");// on stocke la taille de l'image
+                        fprintf(fichier,"5x3 ");// on stocke la taille de l'image
+                        fprintf(fichier," %s\n",s_maintenant);//on stocke le tableau d'heure
                 }else if(typeEcran==3){//  type d'ecran interactif
                         fprintf(fichier,"interactif");//on stocke le type
-                        fprintf(fichier,"position initial de l'avion\n");// on stocke la position in$
-
-        }
+                        fprintf(fichier,"position initial de l'avion");// on stocke la position initial de l'avion
+                        fprintf(fichier," %s\n",s_maintenant);//on stocke le tableau d'heure
+                }
         fclose(fichier);// fermeture du fichier
+    }
 }
-
-
-
-
 #endif // HISTORIQUE_H_INCLUDED
+
